5436d8c1f450e239570948162c88fb7b41fb4f9c
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-30 20:13:14 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,9 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-
+  
+  extend Memorable 
+  
   @@artists = []
 
   def initialize
@@ -19,13 +21,7 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
+ 
 
   def add_song(song)
     @songs << song
